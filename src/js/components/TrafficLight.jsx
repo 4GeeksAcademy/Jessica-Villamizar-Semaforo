e2c7e0f27900161bd17b1f3b938fe6d7446eb2c8
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-20 21:41:38 +0000

    primer desafio

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -10,9 +10,9 @@ const Trafficlight = () => {
 		<div className="container-fluid bg-dark" style={{ width: "20px", height: "100px" }}> </div>
 		<div className="container-fluid bg-dark p-4" style={{ width: "80px", height: "200px" }}>
 			<div class="btn-group-vertical d-flex justify-context align-items" role="group" aria-label="Basic mixed styles example">
-				<button type="button" class="btn btn-danger mb-3 rounded-circle" style={{ width: "40px", height: "40px" }}></button>
-				<button type="button" class="btn btn-warning mb-3 rounded-circle" style={{ width: "40px", height: "40px" }}></button>
-				<button type="button" class="btn btn-success mb-3 rounded-circle" style={{ width: "40px", height: "40px" }}></button>
+				<button onClick={ () => setColor("red")} type="button" class={"btn btn-danger mb-3 rounded-circle " + (color == "red" ? "on" : "" )} style={{ width: "40px", height: "40px" }}></button>
+				<button onClick={ () => setColor("yellow")} type="button" class={"btn btn-warning mb-3 rounded-circle " + (color == "yellow" ? "on" : "" )} style={{ width: "40px", height: "40px" }}></button>
+				<button onClick={ () => setColor("green")} type="button" class={"btn btn-success mb-3 rounded-circle " + (color == "green" ? "on" : "" )}style={{ width: "40px", height: "40px" }}></button>
 			</div>
 		</div>
 	</div>
