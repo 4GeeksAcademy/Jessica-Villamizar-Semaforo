11b35a8871049f91c5e532ed6232b3baac564284
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-21 15:49:22 +0000

    terminado

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,45 +1,59 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Trafficlight = () => {
+const TrafficLight = () => {
 
-	const [color, setColor] = useState("red");
+    const [color, setColor] = useState("red");
+    
+    const [isPurpleOn, setIsPurpleOn] = useState(false);
 
-	const [isPurpleOn, setIsPurpleOn] = useState(false);
+    const handleAddLight = () => {
+        setIsPurpleOn(!isPurpleOn); 
+    };
 
-	const handleAddLigth = () => {
-		setIsPurpleOn(!isPurpleOn);
-	};
+    return (
+        
+        <div className="d-flex flex-column align-items-center mt-4">    
+            <div className="bg-dark" style={{ width: "30px", height: "90px" }}></div>
+            <div className="bg-dark p-4" style={{ width: "120px", height: "auto", borderRadius: "10px" }}>
+                <div className="d-flex flex-column align-items-center">
+                    <button 
+                        onClick={() => setColor("red")} 
+                        type="button" className={"btn btn-danger mb-3 rounded-circle " + (color === "red" ? "onred" : "")} 
+                        style={{ width: "55px", height: "55px" }}
+                    ></button>
 
+                    <button 
+                        onClick={() => setColor("yellow")} 
+                        type="button" className={"btn btn-warning mb-3 rounded-circle " + (color === "yellow" ? "onyellow" : "")} 
+                        style={{ width: "55px", height: "55px" }}
+                    ></button>
 
-	return <div className="d-flex flex-column align-items-center mt-4">
-		<div className="bg-dark" style={{ width: "30px", height: "100px" }}> </div>
-		<div className="bg-dark p-4" style={{ width: "100px", height: "auto", borderRadius: "10px" }}>
-			<div class="d-flex flex-column align-items-center">
+                    <button 
+                        onClick={() => setColor("green")} 
+                        type="button" className={"btn btn-success mb-3 rounded-circle " + (color === "green" ? "ongreen" : "")} 
+                        style={{ width: "55px", height: "55px" }}
+                    ></button>
 
-				<button onClick={() => setColor("red")}
-					type="button" class={"btn btn-danger mb-3 rounded-circle " + (color == "red" ? "onred" : "")}
-					style={{ width: "40px", height: "40px" }}></button>
+                    {isPurpleOn && (
+                        <button 
+                            onClick={() => setColor("purple")} 
+                            type="button" className={"btn btn-purple mb-3 rounded-circle " + (color === "purple" ? "onpurple" : "")} 
+                            style={{ width: "55px", height: "55px", backgroundColor: "#800080", borderColor: "#800080" }}
+                        ></button>
+                    )}
+                </div>
+            </div>
 
-				<button onClick={() => setColor("yellow")}
-					type="button" class={"btn btn-warning mb-3 rounded-circle " + (color == "yellow" ? "onyellow" : "")}
-					style={{ width: "40px", height: "40px" }}></button>
-
-				<button onClick={() => setColor("green")} 
-				type="button" class={"btn btn-success mb-3 rounded-circle " + (color == "green" ? "ongreen" : "")} 
-				style={{ width: "40px", height: "40px" }}></button>
-				
-				{isPurpleOn && (
-				<button onClick={() => setColor("purple")} 
-				type="button" class={"btn btn-purple mb-3 rounded-circle " + (color == "purple" ? "onpurple" : "")} 
-				style={{ width: "40px", height: "40px" }}></button>)}
-			</div>
-		</div>
-		<div className="mt-5" role="group" aria-label="Basic mixed styles example">
-			<button onClick={() => setIsPurpleOn("true")} type="button" class="btn btn-primary btn-lg">Agregar Luz</button>
-		</div>
-	</div>
-
+            <div className="mt-5">
+                <button 
+                    onClick={handleAddLight} type="button" 
+                    className="btn btn-dark btn-lg"
+                >
+                    {isPurpleOn ? "Apagar Luz" : "Enceder Luz"}
+                </button>
+            </div>
+        </div>
+    );
 };
 
-export default Trafficlight;
+export default TrafficLight;
